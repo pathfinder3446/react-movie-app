2f5bd82f235b6dbe3479336479cd27a14f8ec41e
Arif <kanebrian0634@gmail.com>
2022-07-16 17:20:12 +0300

    ..

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
+import {useNavigate} from "react-router-dom";
 const Navbar = () => {
+
+  const navigate = useNavigate();
+
+
   return ( 
       <div className="navbar">
           <div className="navLeft">
-              <h4>React Movie App</h4>
+              <h4 onClick={() => navigate('/')} >React Movie App</h4>
           </div>
           <div className="navRight">
-              <button className="btn" >Login</button>
-              <button className="btn" >Register</button>
+              <button className="btn" onClick={() => navigate('login')} >Login</button>
+              <button className="btn" onClick={() => navigate('register')}  >Register</button>
           </div>
       </div>
    );
